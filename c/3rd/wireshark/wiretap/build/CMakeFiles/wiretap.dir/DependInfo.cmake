ef987fde302ecc06036b92664077e6bca552d857
Ben Morse <bjmorse@gmail.com>
2022-07-17 02:47:53 -0700

    wiretap .a works

---
+++ c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/DependInfo.cmake
@@ -0,0 +1,92 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/5views.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/5views.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/aethra.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/aethra.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/ascendtext.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/ascendtext.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/atm.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/atm.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/ber.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/ber.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/btsnoop.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/btsnoop.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/busmaster.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/busmaster.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/camins.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/camins.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/candump.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/candump.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/capsa.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/capsa.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/catapult_dct2000.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/catapult_dct2000.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/commview.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/commview.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/cosine.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/cosine.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/csids.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/csids.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/daintree-sna.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/daintree-sna.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/dbs-etherwatch.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/dbs-etherwatch.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/dct3trace.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/dct3trace.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/dpa400.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/dpa400.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/erf.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/erf.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/eyesdn.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/eyesdn.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/file_wrappers.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/file_wrappers.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/hcidump.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/hcidump.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/i4btrace.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/i4btrace.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/ipfix.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/ipfix.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/iptrace.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/iptrace.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/iseries.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/iseries.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/json.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/json.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/k12.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/k12.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/lanalyzer.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/lanalyzer.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/libpcap.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/libpcap.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/log3gpp.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/log3gpp.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/logcat.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/logcat.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/logcat_text.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/logcat_text.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/merge.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/merge.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/mime_file.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/mime_file.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/mp2t.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/mp2t.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/mplog.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/mplog.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/netmon.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/netmon.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/netscaler.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/netscaler.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/netscreen.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/netscreen.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/nettl.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/nettl.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/network_instruments.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/network_instruments.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/netxray.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/netxray.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/ngsniffer.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/ngsniffer.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/packetlogger.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/packetlogger.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/pcap-common.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/pcap-common.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/pcapng.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/pcapng.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/peekclassic.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/peekclassic.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/peektagged.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/peektagged.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/pppdump.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/pppdump.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/radcom.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/radcom.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/rfc7468.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/rfc7468.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/ruby_marshal.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/ruby_marshal.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/snoop.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/snoop.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/stanag4607.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/stanag4607.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/systemd_journal.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/systemd_journal.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/tnef.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/tnef.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/toshiba.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/toshiba.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/visual.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/visual.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/vms.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/vms.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/vwr.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/vwr.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/wtap.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/wtap.o"
+  "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/wtap_opttypes.c" "/src/wasm-packet-dissector/c/3rd/wireshark/wiretap/build/CMakeFiles/wiretap.dir/wtap_opttypes.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "WS_BUILD_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../../../zlib-glib-ffi/include/glib-2.0"
+  "../../../zlib-glib-ffi/lib/glib-2.0/include"
+  "../../../wireshark"
+  "../../../wireshark-compat"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
